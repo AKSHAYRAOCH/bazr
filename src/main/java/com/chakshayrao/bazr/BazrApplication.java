b783b444423d415db9acad09dc9786cb89d44c05
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-02-07 12:48:14 +0530

    Initial commit

---
+++ src/main/java/com/chakshayrao/bazr/BazrApplication.java
@@ -0,0 +1,13 @@
+package com.chakshayrao.bazr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chakshayrao/bazr/BazrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chakshayrao.bazr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
