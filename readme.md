4afa87b0b2003b8822a7406715979e63444667d4
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-03-01 19:51:37 +0530

    added readme file

---
+++ readme.md
@@ -0,0 +1,3 @@
+# BazR
+
+#### <em> BazR is a web application that will help you sell digital goods online, this application is built using spring boot framework in Java. 
--- src/main/java/com/chakshayrao/bazr/BazrApplication.java
+++ src/main/java/com/chakshayrao/bazr/BazrApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BazrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BazrApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BazrApplication.class, args);
+    }
 }
